e68af7f8be8d818b2fccfcc5a3344c0cbc3dc964
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-04-12 12:46:39 +0200

    Validaciones dinámicas

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -11,7 +11,7 @@ export class DynamicPageComponent {
 	// });
 
 	public myForm: FormGroup = this.fb.group({
-		name: ['', Validators.required, Validators.minLength(3)],
+		name: ['', [Validators.required, Validators.minLength(3)]],
 		favoriteGames: this.fb.array([
 			['Metal Gear', Validators.required],
 			['Death Stranding', Validators.required],
@@ -20,8 +20,32 @@ export class DynamicPageComponent {
 
 	constructor(private fb: FormBuilder) {}
 
-	get favoriteGAmes() {
+	get favoriteGames() {
 		return this.myForm.get('favoriteGames') as FormArray;
+	}
+
+	isValidField(field: string): boolean | null {
+		return this.myForm.controls[field].errors && this.myForm.controls[field].touched;
+	}
+
+	isValidFieldInArray(formArray: FormArray, index: number) {
+		return formArray.controls[index].errors && formArray.controls[index].touched;
+	}
+
+	getFieldError(field: string): string | null {
+		if (!this.myForm.controls[field]) return null;
+
+		const errors = this.myForm.controls[field].errors || {};
+
+		for (const key of Object.keys(errors)) {
+			switch (key) {
+				case 'required':
+					return 'Este campo es requerido';
+				case 'minlength':
+					return `Mínimo ${errors['minlength'].requiredLength} caracteres.`;
+			}
+		}
+		return null;
 	}
 
 	onSubmit(): void {
