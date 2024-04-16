2234b4f8249d9b58091032a0399a7288729058a6
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-04-16 14:54:27 +0200

    Verificar que dos campos sean iguales

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -8,17 +8,22 @@ import { EmailValidator } from '../../../shared/validators/email-validator.servi
 	templateUrl: './register-page.component.html',
 })
 export class RegisterPageComponent {
-	public myForm = this.fb.group({
-		// name: ['', [Validators.required, Validators.pattern(customValidators.firstNameAndLastnamePattern)]],
-		// email: ['', [Validators.required, Validators.pattern(customValidators.emailPattern)]],
-		// username: ['', [Validators.required, customValidators.cantBeStrider]],
-		name: ['', [Validators.required, Validators.pattern(this.validatorService.firstNameAndLastnamePattern)]],
-		// email: ['', [Validators.required, Validators.pattern(this.validatorService.emailPattern)], [new EmailValidator()]],
-		email: ['', [Validators.required, Validators.pattern(this.validatorService.emailPattern)], [this.emailValidator]],
-		username: ['', [Validators.required, this.validatorService.cantBeStrider]],
-		password: ['', [Validators.required, Validators.minLength(6)]],
-		password2: ['', [Validators.required]],
-	});
+	public myForm = this.fb.group(
+		{
+			// name: ['', [Validators.required, Validators.pattern(customValidators.firstNameAndLastnamePattern)]],
+			// email: ['', [Validators.required, Validators.pattern(customValidators.emailPattern)]],
+			// username: ['', [Validators.required, customValidators.cantBeStrider]],
+			name: ['', [Validators.required, Validators.pattern(this.validatorService.firstNameAndLastnamePattern)]],
+			// email: ['', [Validators.required, Validators.pattern(this.validatorService.emailPattern)], [new EmailValidator()]],
+			email: ['', [Validators.required, Validators.pattern(this.validatorService.emailPattern)], [this.emailValidator]],
+			username: ['', [Validators.required, this.validatorService.cantBeStrider]],
+			password: ['', [Validators.required, Validators.minLength(6)]],
+			password2: ['', [Validators.required]],
+		},
+		{
+			validators: [this.validatorService.isFieldOneEqualFieldTwo('password', 'password2')],
+		}
+	);
 
 	constructor(private fb: FormBuilder, private validatorService: ValidatorService, private emailValidator: EmailValidator) {}
 
