2807c5f2c96e8df6887591b19c919b7f254c5606
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-04-16 12:43:24 +0200

    Validaciones Email y Patrones

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-import { cantBeStrider } from '../../../shared/validators/validators';
+import * as customValidators from '../../../shared/validators/validators';
 
 @Component({
 	templateUrl: './register-page.component.html',
 })
 export class RegisterPageComponent {
 	public myForm = this.fb.group({
-		name: ['', [Validators.required]],
-		email: ['', [Validators.required]],
-		username: ['', [Validators.required, cantBeStrider]],
+		name: ['', [Validators.required, Validators.pattern(customValidators.firstNameAndLastnamePattern)]],
+		email: ['', [Validators.required, Validators.pattern(customValidators.emailPattern)]],
+		username: ['', [Validators.required, customValidators.cantBeStrider]],
 		password: ['', [Validators.required, Validators.minLength(6)]],
 		password2: ['', [Validators.required]],
 	});
